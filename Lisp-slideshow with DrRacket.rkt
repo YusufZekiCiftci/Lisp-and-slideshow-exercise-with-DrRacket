b365f3f90e4cc9c7f8c132c85e8d603f602d7932
Yusuf Zeki Ciftci <yusufzekiciftci@outlook.com>
2023-08-13 18:19:45 +0300

    excercise for Lisp and slideshow with DrRacket

---
+++ Lisp-slideshow with DrRacket.rkt
@@ -0,0 +1,13 @@
+#lang slideshow
+
+(circle 100)
+(rectangle 70 40)
+(hc-append (circle 100) (rectangle 70 40))
+(define a 4)
+(define b 2)
+(+ a b)
+(- a b)
+(/ a b)
+(* a b)
+;^^^^^^^  prefix notasyonu, operatör arglardan önce verilir.
+
